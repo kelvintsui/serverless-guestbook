b4dd0d059c3bd02d6eee25a9c052dfcce7f88657
kelvintsui <tsuichunhing@gmail.com>
2022-04-18 23:06:54 +0800

    Modified apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://82ef7f00.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
